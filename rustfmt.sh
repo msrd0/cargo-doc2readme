5993cbd4ba4edcb0a20d537983cbbb9615680e16
msrd0 <msrd0@users.noreply.github.com>
2022-11-27 21:06:49 +0100

    Add rustfmt.sh script to also format tests (#194)

---
+++ rustfmt.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -euo pipefail
+
+cargo=${CARGO:-cargo}
+version="$($cargo fmt -- -V)"
+case "$version" in
+	*nightly*)
+		# all good, no additional flags required
+		;;
+	*)
+		# assume we're using some sort of rustup setup
+		cargo="$cargo +nightly"
+		;;
+esac
+
+return=0
+while read file; do
+	test=no
+	fail=no
+	ok=yes
+
+	# check if this is a test which is allowed to fail
+	# also, ignore anything in the target folder
+	case "$file" in
+		*/target/*)
+			continue
+			;;
+		*/fail/*)
+			fail=yes
+			test=yes
+			;;
+		*/tests/*)
+			test=yes
+			;;
+		*)
+			;;
+	esac
+
+	echo -e "\e[1m ==> Formatting project $file ...\e[0m"
+
+	# check that the project compiles (unless fail) without modifying the lock file
+	if [ "$test" == "yes" ]; then
+		cargo check --manifest-path "$file" --locked || ok=no
+		if [ "$fail" == "yes" ] && [ "$ok" == "no" ] && ! cargo check --manifest-path "$file" &>/dev/null; then
+			echo -e "\e[1;33m  -> Ignored\e[0m"
+			continue
+		fi
+	fi
+
+	# run rustfmt with the provided flags
+	if [ "$ok" == "yes" ]; then
+		$cargo fmt --manifest-path "$file" -- \
+			--config-path "$(dirname "$0")/rustfmt.toml" "$@" \
+			|| ok=no
+	fi
+
+	if [ "$ok" == "yes" ]; then
+		echo -e "\e[1;32m  -> Success\e[0m"
+	else
+		echo -e "\e[1;31m  -> Failed\e[0m"
+		return=1
+	fi
+done < <(find . -name 'Cargo.toml' -type f)
+
+exit $return
--- tests/pass/nested-lists/lib.rs
+++ tests/pass/nested-lists/lib.rs
@@ -1,12 +1,12 @@
 //! Several items:
-//! 
+//!
 //! 1. Item one
 //! 1. Item two
 //!     1. Subitem one
 //!     1. Subitem two
 //! 1. Item three
-//! 
+//!
 //! Again:
-//! 
+//!
 //! 2. Item one starting from two
 //! 2. Item two starting from two
