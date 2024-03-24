f524239e7a9090dc68e7eb06b8813a05956337cb
Dominic <git@msrd0.de>
2024-03-24 19:05:26 +0100

    rustfmt

--- tests/pass/code-block-with-hidden-lines/lib.rs
+++ tests/pass/code-block-with-hidden-lines/lib.rs
@@ -1,14 +1,14 @@
 //! This is an example showcasing code blocks with hidden lines
-//! 
+//!
 //! Random lines with # will not be altered.
-//! 
+//!
 //! - The same goes for lines in lists starting with a sharp:
 //! - # this line is not hidden
-//! 
+//!
 //! ```
 //! // But in code blocks, lines starting with a # are hidden:
 //! # // You can't see me
-//! 
+//!
 //! // But not when those lines are actually relevant:
 //! #[no_std]
 //! ```
