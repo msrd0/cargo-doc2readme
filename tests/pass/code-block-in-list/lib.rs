6373d7f5cb7c3fd565d229983897ecda7d96dba0
Dominic <git@msrd0.de>
2024-05-13 14:53:28 +0200

    add failing test

---
+++ tests/pass/code-block-in-list/lib.rs
@@ -0,0 +1,12 @@
+//! This is an example testing code blocks in lists
+//! 
+//!   - in the past, this has caused issues
+//!   - one of such cases was [#440](https://github.com/msrd0/cargo-doc2readme/issues/440)
+//!   - therefore, we have this example/test:
+//!     
+//!     ```rust
+//!     # #[derive(Clone, Copy)]
+//!     struct Foo {
+//!         bar: usize
+//!     }
+//!     ```
