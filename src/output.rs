caf5128ff1266b30e020ec5ceca1a24ce393126e
Dominic <git@msrd0.de>
2024-07-24 22:51:45 +0200

    Partially revert "Support GitHub alerts (#434)"
    Turns out pulldown-cmark has built-in support since 0.11
    
    This partially reverts commit 5fcc063c61b63b4ab8c1dd125db8847a5dc1d377.

--- src/output.rs
+++ src/output.rs
@@ -111,29 +111,12 @@ fn is_hidden_codeblock_line(line: &str) -> bool {
 		|| (line.starts_with('#') && line.chars().nth(1).unwrap_or('a').is_whitespace())
 }
 
-fn is_alert(link_type: LinkType, dest_url: &CowStr, title: &CowStr, id: &CowStr) -> bool {
-	if link_type != LinkType::ShortcutUnknown || !dest_url.is_empty() || title != id {
-		return false;
-	}
-	is_alert_tag(title)
-}
-
-fn is_alert_tag(tag: &str) -> bool {
-	tag == "!NOTE"
-		|| tag == "!TIP"
-		|| tag == "!IMPORTANT"
-		|| tag == "!WARNING"
-		|| tag == "!CAUTION"
-}
-
 struct EventFilter<'a, I: Iterator<Item = Event<'a>>> {
 	iter: I,
 	links: &'a mut BTreeMap<String, String>,
 
 	in_code_block: bool,
 	in_code_block_ignored: bool,
-	block_quote_level: usize,
-	inside_github_alert: bool,
 	link_idx: usize
 }
 
@@ -145,8 +128,6 @@ impl<'a, I: Iterator<Item = Event<'a>>> EventFilter<'a, I> {
 
 			in_code_block: false,
 			in_code_block_ignored: false,
-			block_quote_level: 0,
-			inside_github_alert: false,
 			link_idx: 0
 		}
 	}
@@ -206,26 +187,6 @@ impl<'a, I: Iterator<Item = Event<'a>>> Iterator for EventFilter<'a, I> {
 						}))
 					},
 
-					Tag::BlockQuote => {
-						// block quotes can be nested
-						self.block_quote_level += 1;
-						Tag::BlockQuote
-					},
-
-					Tag::Link {
-						link_type,
-						dest_url,
-						title,
-						id
-					} if self.block_quote_level > 0
-						&& is_alert(link_type, &dest_url, &title, &id) =>
-					{
-						debug_assert!(!self.inside_github_alert, "nested github alerts");
-						self.inside_github_alert = true;
-						// emit HTML to prevent escaping of text
-						return Some(Event::InlineHtml(format!("[{}]", title).into()));
-					},
-
 					Tag::Link {
 						link_type,
 						dest_url,
@@ -279,16 +240,7 @@ impl<'a, I: Iterator<Item = Event<'a>>> Iterator for EventFilter<'a, I> {
 					tag => tag
 				}),
 
-				// When we are inside a GitHub alert, we get three events:
-				// Start(Tag::Link), Text(<"link" text>), End(Tag::Link).
-				// Since we already emitted all the events we need to correctly
-				// render the alert from the Start event, we are skipping the
-				// Text and End events as long as we are inside the alert.
-				Event::Text(_) if self.inside_github_alert => {
-					continue;
-				},
-
-				Event::End(end_tag) => Event::End(match end_tag {
+				Event::End(tag) => Event::End(match tag {
 					// we record when a codeblock ends
 					TagEnd::CodeBlock => {
 						debug_assert!(
@@ -298,18 +250,6 @@ impl<'a, I: Iterator<Item = Event<'a>>> Iterator for EventFilter<'a, I> {
 						self.in_code_block = false;
 						self.in_code_block_ignored = false;
 						TagEnd::CodeBlock
-					},
-					TagEnd::BlockQuote => {
-						debug_assert!(
-							self.block_quote_level > 0,
-							"Ending a non-started block quote, wtf???"
-						);
-						self.block_quote_level -= 1;
-						TagEnd::BlockQuote
-					},
-					TagEnd::Link if self.inside_github_alert => {
-						self.inside_github_alert = false;
-						continue;
 					},
 					// we don't need to modify any other tags
 					tag => tag
